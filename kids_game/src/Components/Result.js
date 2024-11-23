5247f64fc3eb456c6d6ed7225f0525ddf4127910
piyumalBhathiya <139671996+piyumalBhathiya@users.noreply.github.com>
2024-11-23 17:15:39 +0000

    Add styles to result

--- kids_game/src/Components/Result.js
+++ kids_game/src/Components/Result.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function Result({ result, onPlayAgain }) {
   return (
-    <div>
+    <div style={styles.container}>
       {result && <h3>{result}</h3>}
-      <button onClick={onPlayAgain}>
+      <button onClick={onPlayAgain} style={styles.button}>
         Play Again
       </button>
     </div>
   );
 }
+
+const styles = {
+    container: { marginTop: "20px" },
+    button: { padding: "10px 20px", cursor: "pointer", borderRadius: "4px" },
+  };
+  
